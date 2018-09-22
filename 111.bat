d3013ac560f8f83b9d42a3e3057b54a11592a381
temp <temp@STUD.UNV.SU>
2018-09-22 14:33:48 +0300

    lab 22/09/2018

--- 111.bat
+++ 111.bat
@@ -1,4 +1,10 @@
-git add *.cpp *.h *.sln *.vcxproj *.user *.filters *.bat
+git add *.cpp 
+git add *.h 
+git add *.sln
+git add  *.vcxproj 
+git add *.user 
+ git add *.filters 
+ git add *.bat
 git commit -am "lab 22/09/2018"
 git push nemo master
 pause
