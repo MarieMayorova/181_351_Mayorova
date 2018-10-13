a64e584eded19cef9b6e415ee433d9063e2b90c8
Майорова Мария Васильевна <m.v.majorova@stud.unv.su>
2018-10-13 16:26:42 +0300

    lab 13.10.2018 16:26:42,38

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -16,8 +16,14 @@ public:
 	bool transp();
 	bool print();
 	bool input();
-	int getRows();
-	int getColums();
+	int getRows()
+	{
+		return rows;
+	}
+	int getColums() 
+	{
+		return colums;
+	}
 	double getElem(int row, int col){
 		if (row < rows && col < colums && row>-1 && col>-1) { return matr[row][col]; }
 		else {
@@ -27,19 +33,63 @@ public:
 	};
 
 };
-int main()
+
+matrix::matrix()
 {
-    return 0;
+	for (int row = 0; row < 10; row++) {
+		for (int col = 0; col < 10; col++)
+		{
+			matr[row][col]=0;
+		}
+	}
+}
+matrix::~matrix()
+{
+}
+bool matrix::sumMatrix(matrix matr2)
+{
+	if (rows != matr2.getRows())//Сравнить количестве строк текущей матрицы с количеством строк matr2
+	{
+		return false;
+	}
+	//Сравнить количестве столбцов текущей матрицы с количеством строк matr2
+
+	//Для каждого элемента matr[row][col] += matr2.getElem(row,col)
+	return true;
+}
+bool matrix::transp()
+{
+	int res[10][10];
+
+	return false;
 }
 bool  matrix::input()
 {
-	std::cin >> row;
-	if (row > rows) { std::cout << "Error!\n"; return false; };
-	std::cin >> col;
-	if (col > colums) { std::cout << "Error!"; return false; };
+	std::cin >> rows;
+	if (1 > rows || rows>10) { std::cout << "Error!\n"; return false; };
+	std::cin >> colums;
+	if (1 > colums || colums>10) { std::cout << "Error!\n"; return false; };
 
-	std::cin >> matr[row][col] >> std::endl;
-
-
+	for (int row = 0; row < rows; row++) {
+		for (int col = 0; col < colums; col++)
+		{
+			std::cin >> matr[row][col];
+		}
+	}
 	return true;
+}
+
+int main()
+{
+	matrix matA, matB;
+	matA.input();
+	matB.input();
+	
+	matA.transp();
+	matA.print();
+
+	matA.sumMatrix(matB); //matA = matA + matB
+	matA.print();
+
+	return 0;
 }
